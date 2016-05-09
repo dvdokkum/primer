c844c4ed1279e777ca21655d6bd5daf78c43e073
dvd <david@chartbeat.com>
2016-05-09 00:28:42 -0400

    setup basic spotify auth and save key to local storage

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,21 @@
+//create primer angular module
 var primer = angular.module('primer', ['spotify']);
-console.log("test");
 
-primer.controller('test', function(Spotify) {
-
-Spotify.getAlbum('0sNOF9WDwhWunNAHPD3Baj').then(function (data) {
-  console.log(data);
+//set auth configs for spotify
+primer.config(function (SpotifyProvider) {
+  SpotifyProvider.setClientId('de8f8891bad644ef880053577e18cd35');
+  SpotifyProvider.setRedirectUri('http://localhost:3000/callback.html');
+  SpotifyProvider.setScope('user-top-read user-library-read user-follow-read playlist-read-private');
 });
 
-});
+primer.controller('main', ['$scope', 'Spotify', function($scope, Spotify) {
 
+	Spotify.getAlbum('0sNOF9WDwhWunNAHPD3Baj').then(function (data) {
+  	console.log(data);
+	});
+
+	$scope.login = function() {
+		Spotify.login();
+	};
+
+}]);
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  response.sendFile( 'index.html');
+  response.sendFile('index.html');
 });
 
 module.exports = router;
